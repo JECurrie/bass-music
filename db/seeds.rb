f3094c4afc5356590675846e7ccce6a1984e8055
Joan Currie <jecurrie8@gmail.com>
2020-10-16 13:40:07 -0400

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 a1 = Album.create(cover: "http://s3.amazonaws.com/codecademy-content/projects/3/bass-music/my-beautiful-dark-twisted-fantasy.jpg", title: "My Beautiful Dark Twisted Fantasy", artist: "Kanye West")
 Track.create(name: "Dark Fantasy", minutes: 5, album_id: a1.id)
 Track.create(name: "Gorgeous", minutes: 6, album_id: a1.id)
